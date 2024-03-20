d718abd5a7e579a97f52a4184ebaf0acbfa3a2a5
Maksim Belfer <maxbelf1@gmail.com>
2024-03-21 00:31:28 +0300

    micro_frontend initial

---
+++ header/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+  <div class="content">
+   <h1>{{ msg }}</h1>
+  </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      msg: 'VUE 2'
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
